214dba9d078c9c15e50a8911451ceffef4edf08c
Cremacious <108367916+Cremacious@users.noreply.github.com>
2023-09-23 16:10:31 -0400

    GUI Added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os
-
+import PySimpleGUI as sg
 import requests
+import os
 
 url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
 MY_KEY = os.environ.get("KEY")
+
 def translate(text):
     payload = {
         "q": f"{text}",
@@ -18,13 +19,32 @@ def translate(text):
     }
 
     response = requests.post(url, data=payload, headers=headers)
-    result = response.json()
-    new_text = result['data']['translations'][0]['translatedText']
-    return new_text
+    data = response.json()
+    translated_text = data['data']['translations'][0]['translatedText']
+    return translated_text
 
+# Define the layout for the window
+layout = [
+    [sg.Text('Enter text to translate:', size=(20, 1))],
+    [sg.InputText('', size=(30, 1), key='-INPUT-')],
+    [sg.Text('Translation:', size=(20, 1))],
+    [sg.InputText('', size=(30, 1), key='-OUTPUT-')],
+    [sg.Button('Translate')],
+]
 
-running = True
-while running:
-    text = input("Text: ")
-    result = translate(text)
-    print(result)
+# Create the window
+window = sg.Window('English to Spanish Translator', layout)
+
+# Event loop
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+    if event == 'Translate':
+        input_text = values['-INPUT-']
+        translated_text = translate(input_text)
+        window['-OUTPUT-'].update(translated_text)
+
+# Close the window
+window.close()
